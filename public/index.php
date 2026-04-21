c8ad9b8f9ee8bdf96509516f233fc5284259e686
your Name <you@example.com>
2026-04-21 13:10:09 +0300

    Step 8: Move customer logic to CustomerController

--- public/index.php
+++ public/index.php
@@ -2,37 +2,11 @@
 
 <?php
 require __DIR__ . '/../db/connect.php';
+require __DIR__ . '/../src/controllers/CustomerController.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($requestUri === '/customers') {
-    $sql = "SELECT * FROM customers";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        echo "<h2>Klientu saraksts</h2>";
-        echo "<table border='1'>
-                <tr>
-                    <th>ID</th>
-                    <th>Vārds</th>
-                    <th>Uzvārds</th>
-                    <th>E-pasts</th>
-                    <th>Dzimšanas datums</th>
-                    <th>Punkti</th>
-                </tr>";
-        while($row = $result->fetch_assoc()) {
-            echo "<tr>
-                    <td>" . $row["customer_id"] . "</td>
-                    <td>" . $row["name"] . "</td>
-                    <td>" . $row["last_name"] . "</td>
-                    <td>" . $row["email"] . "</td>
-                    <td>" . $row["birthday"] . "</td>
-                    <td>" . $row["points"] . "</td>
-                  </tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "Nav atrasts neviens klients.";
-    }
+    echo CustomerController::index($conn);
 }
 ?>
---
+++ src/controllers/CustomerController.php
@@ -0,0 +1,36 @@
+<?php
+
+class CustomerController {
+    public static function index($conn) {
+        $sql = "SELECT * FROM customers";
+        $result = $conn->query($sql);
+
+        $output = "<h2>Klientu saraksts</h2>";
+        if ($result->num_rows > 0) {
+            $output .= "<table border='1'>
+                    <tr>
+                        <th>ID</th>
+                        <th>Vārds</th>
+                        <th>Uzvārds</th>
+                        <th>E-pasts</th>
+                        <th>Dzimšanas datums</th>
+                        <th>Punkti</th>
+                    </tr>";
+            while($row = $result->fetch_assoc()) {
+                $output .= "<tr>
+                        <td>" . $row["customer_id"] . "</td>
+                        <td>" . $row["name"] . "</td>
+                        <td>" . $row["last_name"] . "</td>
+                        <td>" . $row["email"] . "</td>
+                        <td>" . $row["birthday"] . "</td>
+                        <td>" . $row["points"] . "</td>
+                      </tr>";
+            }
+            $output .= "</table>";
+        } else {
+            $output .= "Nav atrasts neviens klients.";
+        }
+        return $output;
+    }
+}
+?>
