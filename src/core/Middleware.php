ec694b8fb30e83b7f2b8e2616a36b2bd21f99330
your Name <you@example.com>
2026-04-23 08:52:53 +0300

    Architecture: Add Middleware support to the Router

---
+++ src/core/Middleware.php
@@ -0,0 +1,5 @@
+<?php
+
+abstract class Middleware {
+    abstract public function handle($container);
+}
--- src/core/Router.php
+++ src/core/Router.php
@@ -8,17 +8,28 @@ class Router {
         $this->container = $container;
     }
 
-    public function add($uri, $controller, $method) {
+    public function add($uri, $controller, $method, $middleware = []) {
         $this->routes[$uri] = [
             'controller' => $controller,
-            'method' => $method
+            'method' => $method,
+            'middleware' => $middleware
         ];
     }
 
     public function dispatch($uri) {
         if (array_key_exists($uri, $this->routes)) {
-            $controllerName = $this->routes[$uri]['controller'];
-            $methodName = $this->routes[$uri]['method'];
+            $route = $this->routes[$uri];
+            
+            // Izpildām Middleware
+            foreach ($route['middleware'] as $middlewareClass) {
+                if (class_exists($middlewareClass)) {
+                    $middleware = new $middlewareClass();
+                    $middleware->handle($this->container);
+                }
+            }
+
+            $controllerName = $route['controller'];
+            $methodName = $route['method'];
 
             if (class_exists($controllerName)) {
                 $controller = new $controllerName($this->container);
